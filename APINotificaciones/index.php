71477806b846f9c90a2f4861952b9ea309505abf
Sergio Lizárraga <90207380+SergioUPT@users.noreply.github.com>
2023-04-29 16:34:26 -0500

    v2

--- APINotificaciones/index.php
+++ APINotificaciones/index.php
@@ -2,20 +2,35 @@
 
 require 'flight/Flight.php';
 
+Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=bdapi','root',''));
+
 Flight::route('GET /Cnotfound', function () {
     Flight::json("Cliente no encontrado");
 });
 
 Flight::route('POST /Cnotfound', function () {
-    $Cnotfound=(Flight::request()->data->Cnotfound);
-
-    if($Cnotfound=="CNF")
+    $buscar=(Flight::request()->data->buscar);
+    $tipo_msg=(Flight::request()->data->tipo_msg);
+    $sentencia=Flight::db()->prepare("SELECT * FROM tbcliente WHERE id='$buscar'");
+    $sentencia->execute();
+    $datos=$sentencia->fetchAll();
+    if((count($datos)!=0) && ($tipo_msg=="SF"))
     {
-        Flight::json("Cliente no encontrado");
+        $id=$datos[0];
+        $sql="INSERT INTO tbregistro (idcliente,fecha,tipo) VALUES('$id',NOW(),'$tipo_msg')";
+        $sentencia1=Flight::db()->prepare($sql);
+        $sentencia1->execute();
+        
+    }
+    else if((count($datos)!=0) && ($tipo_msg=="RF"))
+    {
+        $id=$datos[0];
+        $tipo_msg2="Retiro de Fondos";
+        Flight::json($id);
     }
     else
     {
-        Flight::json("ERROR");
+        Flight::json("Cliente no encontrado");  
     }
 });
 
